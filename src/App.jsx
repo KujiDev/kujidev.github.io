60b907c29c15a2e93e4521a142016acd6ba83d6b
KujiDev <sofronischristos@gmail.com>
2025-11-27 02:57:12 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,6 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import { CameraControls, Environment, MeshPortalMaterial, PerspectiveCamera, Text, useCursor } from "@react-three/drei";
-import * as THREE from "three";
-import { useRef, useState } from "react";
-
-function Frame({ id, name, author, bg, border, width = 1, height = 1.61803398875, children, ...props }) {
-  const portal = useRef()
-  const [enter, setEnter] = useState(false)
-  const [hovered, hover] = useState(false)
-  useCursor(hovered)
-  useFrame((_, dt) => {
-    if (!portal.current) return;
-
-    const duration = .3;
-    const current = portal.current.blend;
-    const delta = +enter - current;
-
-    if (Math.abs(delta) > 0.0001) {
-      const step = (dt / duration) * Math.sign(delta);
-      const next = current + step;
-
-      portal.current.blend =
-        delta > 0 ? Math.min(next, +enter) : Math.max(next, +enter);
-    } else {
-      portal.current.blend = +enter;
-    }
-  })
-  return (
-    <group {...props}>
-      <Text fontSize={0.3} anchorY="top" anchorX="left" lineHeight={0.8} position={[-0.375, 0.715, 0.01]} material-toneMapped={false}>
-        {name}
-      </Text>
-      <Text fontSize={0.1} anchorX="right" position={[0.4, -0.659, 0.01]} material-toneMapped={false}>
-        /{id}
-      </Text>
-      <Text fontSize={0.04} anchorX="right" position={[0.0, -0.677, 0.01]} material-toneMapped={false}>
-        {author}
-      </Text>
-      <mesh position={[0, 0, -0.0001]}>
-        <planeGeometry args={[width + 0.1, height + 0.1]} />
-        <meshBasicMaterial color={border} />
-      </mesh>
-      <mesh name={id} onDoubleClick={(e) => (
-        e.stopPropagation(),
-        setEnter(prev => !prev)
-      )} onPointerOver={(e) => hover(true)} onPointerOut={() => hover(false)}>
-        <planeGeometry args={[width, height]} />
-        <MeshPortalMaterial ref={portal}
-          side={THREE.DoubleSide}>
-          <color attach="background" args={[bg]} />
-          {children}
-        </MeshPortalMaterial>
-      </mesh>
-    </group>
-  )
-}
+import { Canvas } from "@react-three/fiber";
+import { CameraControls, Environment } from "@react-three/drei";
+import Frame from "@/components/Frame";
 
 export default function App() {
   return (
